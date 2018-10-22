54e6b6edd1f47fb035c11dcb6ae95d43cacfbf6a
William R Sweeney <wrsweeney2@gmail.com>
2018-10-22 12:02:09 -0400

    overhaul to integrate with rest of package

--- src/pc.jl
+++ src/pc.jl
@@ -7,21 +7,20 @@ falls within the cell defined by `lattice`
 
 2. similar to (1) but can build out of a Regions object instead.
 """
-function build_pc(lattice::Bravais, domain::Domain)
-    return build_pc(lattice, Regions=Regions(domain);
+function build_pc_domain(lattice::Bravais, domain::Domain)
+    return build_pc_domain(lattice, Regions=Regions(domain);
                     is_in_domain=domain.is_in_domain, domain_params=domain.domain_params,
                     n₁=domain.n₁, n₂=domain.n₂, F=domain.F)
 end
-function build_pc(lattice::Bravais, regions::Regions=Regions();
+function build_pc_domain(lattice::Bravais, regions::Regions=Regions();
                 is_in_domain=whole_domain, domain_params=[], n₁=1, n₂=0, F=0)
     return build_domain(regions; is_in_domain=is_in_domain, domain_params=domain_params,
                             n₁=n₁, n₂=n₂, F=F, domain_type=:pc, lattice=lattice)
 end
 
 
-
 """
-    domain = add_circle(domain, reference_point: R, x0=0, y0=0, n₁=1, n₂=0, F=0)
+    domain = add_circle_to_pc(pc_domain, reference_point: R, x0=0, y0=0, n₁=1, n₂=0, F=0)
 
 add a circular region to photonic crystal domain, referenced to `reference_point`,
 which can be one of :center, :corner or :corners, :bottom_edge or :bottom or :top_edge
@@ -31,90 +30,94 @@ or :top, :left_edge or :left or :right_edge or :right, :southwest, :south, :sout
 this creates a circle which wraps around the edges of the cell, so that the final
 structure has circles, not pie-wedges
 """
-function add_circle(domain::Domain, reference::Symbol; R, x0=0, y0=0, n₁=1, n₂=0, F=0)
-    v1 = domain.lattice.v1
-    v2 = domain.lattice.v2
-    a = domain.lattice.a
-    b = domain.lattice.b
+function add_circle_to_pc(pc_domain::Domain, reference::Symbol; R, x0=0, y0=0, n₁=1, n₂=0, F=0)
+
+    v1 = pc_domain.lattice.v1
+    v2 = pc_domain.lattice.v2
+    a = pc_domain.lattice.a
+    b = pc_domain.lattice.b
 
     if reference == :center
         x0_new = x0 - v1[1]*a/2 + v2[1]*b/2
         y0_new = y0 - v1[2]*a/2 + v2[2]*b/2
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = x0 + v1[1]*a/2 - v2[1]*b/2
         y0_new = y0 + v1[2]*a/2 - v2[2]*b/2
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = x0 + 3v1[1]*a/2 + v2[1]*b/2
         y0_new = y0 + 3v1[2]*a/2 + v2[2]*b/2
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = x0 + v1[1]*a/2 + 3v2[1]*b/2
         y0_new = y0 + v1[2]*a/2 + 3v2[2]*b/2
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = x0 + v1[1]*a/2 + v2[1]*b/2
         y0_new = y0 + v1[2]*a/2 + v2[2]*b/2
     elseif reference ∈ [:corner, :corners]
         x0_new = v1[1]*a
         y0_new = v1[2]*a
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = v2[1]*b
         y0_new = v2[2]*b
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = v1[1]*a + v2[1]*b
         y0_new = v1[2]*a + v2[2]*b
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = 0
         y0_new=0
     elseif reference ∈ [:bottom_edge, :top_edge, :bottom, :top]
         x0_new = v1[1]*a/2 + v2[1]*b
         y0_new = v1[2]*a/2 + v2[2]*b
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = v1[1]*a/2
         y0_new = v1[2]*a/2
     elseif reference ∈ [:left_edge, :right_edge, :left, :right]
         x0_new = v1[1]*a + v2[1]*b/2
         y0_new = v1[2]*a + v2[2]*b/2
-        domain = add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+        pc_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
 
         x0_new = v2[1]*b/2
         y0_new = v2[2]*b/2
-    elseif reference == :southwest
+    elseif reference ∈ [:southwest, :sw]
         x0_new = x0
         y0_new = y0
-    elseif reference == :south
+    elseif reference ∈ [:south, :s]
         x0_new = x0 + v1[1]*a/2
         y0_new = y0 + v1[1]*a/2
-    elseif reference == :southeast
+    elseif reference ∈ [:southeast, :se]
         x0_new = x0 + v1[1]*a
         y0_new = y0 + v1[2]*a
-    elseif reference ∈ :east
+    elseif reference ∈ [:east, :e]
         x0_new = x0 + v1[1]*a + v2[1]*b/2
         y0_new = y0 + v1[2]*a + v2[2]*b/2
-    elseif reference == :northeast
+    elseif reference ∈ [:northeast, :ne]
         x0_new = x0 + v1[1]*a + v2[1]*b
         y0_new = y0 + v1[2]*a + v2[2]*b
-    elseif reference == :north
+    elseif reference ∈ [:north, :n]
         x0_new = x0 + v1[1]*a/2 + v2[1]*b
         y0_new = y0 + v1[2]*a/2 + v2[2]*b
-    elseif reference == :northwest
+    elseif reference ∈ [:northwest, :nw]
         x0_new = x0 + v2[1]*b
         y0_new = y0 + v2[2]*b
-    elseif reference == :west
+    elseif reference ∈ [:west, :w]
         x0_new = x0 + v2[1]*b/2
         y0_new = y0 + v2[2]*b/2
     else
-        throw(ArgumentError("Invalid unit cell position reference $(reference).
-        Should be one of :center, :corner or a cardinal direction"))
+        throw(ArgumentError("invalid unit cell position reference $(reference).
+        Should be one of :center, :corner or a compass direction"))
     end
-    return add_circle(domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+    new_domain = add_circle_region(pc_domain; R=R, x0=x0_new, y0=y0_new, n₁=n₁, n₂=n₂, F=F)
+
+    return new_domain
 end
+
 
 """
     1. defect_domain = add_defect(domain, Na, Nb, regions=Region())
@@ -128,20 +131,22 @@ crystal `domain` (referenced to the `x0`, `y0` fields of `dom.lattice`)
 in both cases the interior of the defect is built from `regions`
 
 """
-function add_defect(domain::Domain, Na::Int, Nb::Int, regions::Regions=Regions())
-    defect_domain = add_line_defect(domain, :x, Na, Na, Nb, regions)
+function defect_domain(pc_domain::Domain, Na::Int, Nb::Int, regions::Regions=Regions())
+    defect_domain = line_defect_domain(pc_domain, :x, Na, Na, Nb, regions)
     return defect_domain
 end
-function add_defect(domain::Domain, Na::Int, Nb::Array{Int}, regions::Regions=Regions())
-    return add_defect.(Ref(domain), Na, Nb, Ref(regions))
+function defect_domain(pc_domain::Domain, Na::Int, Nb::Array{Int}, regions::Regions=Regions())
+    return defect_domain.(Ref(pc_domain), Na, Nb, Ref(regions))
 end
-function add_defect(domain::Domain, Na::Array{Int}, Nb, regions::Regions=Regions())
-    return add_defect.(Ref(domain), Na, Nb, Ref(regions))
+function defect_domain(pc_domain::Domain, Na::Array{Int}, Nb, regions::Regions=Regions())
+    return defect_domain.(Ref(pc_domain), Na, Nb, Ref(regions))
 end
+
+
 function add_defect(sys::System, domain_num, Na, Nb, regions::Regions=Regions())
     domains = sys.domains
-    defect_domain = add_defect(domains[domain_num], Na, Nb, regions)
-    return System([defect_domain, domains])
+    defect = defect_domain(domains[domain_num], Na, Nb, regions)
+    return System(vcat(defect, domains))
 end
 
 
@@ -161,7 +166,8 @@ Optional arguments:
 
 `N_width` sets the width (in the transverse direction) of the line defect
 """
-function add_line_defect(domain::Domain, direction, N_start, N_stop, N_transverse, regions::Regions=Regions(); N_step=1, N_width=1)
+function line_defect_domain(pc_domain::Domain, direction, N_start, N_stop, N_transverse, regions::Regions=Regions(); N_step=1, N_width=1)
+
     if direction == :x
         Na_start = N_start
         Na_step = N_step
@@ -177,20 +183,20 @@ function add_line_defect(domain::Domain, direction, N_start, N_stop, N_transvers
         Na_step = 1
         Na_stop = N_transverse+N_width-1
     else
-        throw(ArgumentError("Invalid direction $(direction)."))
+        throw(ArgumentError("invalid direction $(direction)."))
     end
-    lattice = domain.lattice
-    n₁ = domain.n₁
-    n₂ = domain.n₂
-    F = domain.F
+    lattice = pc_domain.lattice
+    n₁ = pc_domain.n₁
+    n₂ = pc_domain.n₂
+    F = pc_domain.F
 
     if Na_start==Na_stop && Nb_start==Nb_stop
-        defect_type = :point_defect
+        defect_type = :site_defect
     else
         if N_step > 1
-            defect_type = :periodic_line_defect
+            defect_type = :line_defect_periodic
         else
-            defect_type = :simple_line_defect
+            defect_type = :line_defect_simple
         end
     end
 
@@ -202,6 +208,7 @@ function add_line_defect(domain::Domain, direction, N_start, N_stop, N_transvers
     defect_domain = build_domain(regions; is_in_domain=is_in_cell,
                         domain_params=[Na_start, Na_stop, Na_step, Nb_start, Nb_stop, Nb_step],
                         n₁=n₁, n₂=n₂, F=F, domain_type=defect_type, lattice=lattice)
+
     return defect_domain
 end
 
@@ -214,6 +221,7 @@ is the point (`x`,`y`) in `sys.domains[domain_number]`?
 `bnd` is just along for the ride
 """
 function is_in_cell(x, y, idx, bnd::Boundary, sys::System)
+
     Na_start = Int(sys.domains[idx].domain_params[1])
     Na_stop = Int(sys.domains[idx].domain_params[2])
     Na_step = Int(sys.domains[idx].domain_params[3])
