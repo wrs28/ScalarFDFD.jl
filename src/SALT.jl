84f8eee7ec9ff62b6b44f8519b42e95ebf1a8fa1
William R Sweeney <wrsweeney2@gmail.com>
2019-03-18 14:41:29 -0400

    rm SALT

--- src/SALT.jl
+++
@@ -1,8 +0,0 @@
-#TODO: other SALT specific routines here
-
-"""
-    γ(sim, k) is the lorentzian gain curve
-"""
-function γ(sim::Simulation, k)::ComplexF64
-    return sim.tls.γp/(k-sim.tls.k₀+1im*sim.tls.γp)
-end
